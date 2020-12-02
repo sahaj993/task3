3edb26dbacbbf24aa3f5ca48a07e6b1087f2f343
sahaj993 <sahajseth1998@gmail.com>
2020-12-03 01:15:28 +0530

    Folder structure set up

--- index.js
+++ index.js
@@ -2,6 +2,12 @@ const express = require('express');
 const app = express();
 const port = 9000;
 
+app.set('view engine', 'ejs');
+app.set('views', './view');
+
+// Entry point for all the routes
+app.use('/', require('./routes'));
+
 app.listen(port, function(err){
     if (err){
         console.log('Error in running the server');
